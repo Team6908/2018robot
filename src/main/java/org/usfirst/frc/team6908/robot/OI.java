992c941b9ae4d810c95dd75cb6b7b89462bc6469
Super-Beaver <yuval.sherman1@gmail.com>
2018-01-06 19:34:46 -0500

    changed to commands.* added Rohini's joystick declaration

--- src/main/java/org/usfirst/frc/team6908/robot/OI.java
+++ src/main/java/org/usfirst/frc/team6908/robot/OI.java
@@ -3,7 +3,7 @@ package org.usfirst.frc.team6908.robot;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
 
-import org.usfirst.frc.team6908.robot.commands.ExampleCommand;
+import org.usfirst.frc.team6908.robot.commands.*;
 
 /**
  * This class is the glue that binds the controls on the physical operator
