20b0e30ac9cdc3ab2ec37ef2f0e71d82c0e21aa8
David <treyhiser@gmail.com>
2018-01-22 16:36:05 -0500

    Initial Commit

---
+++ src/main/java/org/usfirst/frc/team6908/robot/commands/AutoDrive.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team6908.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class AutoDrive extends Command {
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
