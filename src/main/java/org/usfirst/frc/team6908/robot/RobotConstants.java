340214b546b15a4ca9e6a49ec1301b2fe414eb92
rohinisharma <rasberry314@gmail.com>
2018-01-27 19:13:34 -0500

    Added comments

--- src/main/java/org/usfirst/frc/team6908/robot/RobotConstants.java
+++ src/main/java/org/usfirst/frc/team6908/robot/RobotConstants.java
@@ -1,7 +1,7 @@
 package org.usfirst.frc.team6908.robot;
 
 public class RobotConstants {
-	public static final double minFlywheelThrottle = -1;
-	public static final double maxFlywheelThrottle = 1;
+	public static final double minFlywheelThrottle = -1;//Speed for outwards push
+	public static final double maxFlywheelThrottle = 1;//Speed for inwards pull
 	public static boolean fondleVector = true;
 }
--- src/main/java/org/usfirst/frc/team6908/robot/subsystems/Fondle.java
+++ src/main/java/org/usfirst/frc/team6908/robot/subsystems/Fondle.java
@@ -20,17 +20,17 @@ public class Fondle extends Subsystem {
     }
     
     public void fondleIn() {
-    	RobotMap.leftFlywheel.set(RobotConstants.maxFlywheelThrottle);
+    	RobotMap.leftFlywheel.set(RobotConstants.maxFlywheelThrottle); //Set intake spinning towards robot
    		RobotMap.rightFlywheel.set(RobotConstants.minFlywheelThrottle);
     }
     
     public void fondleOut() {
-    	RobotMap.leftFlywheel.set(RobotConstants.minFlywheelThrottle);
+    	RobotMap.leftFlywheel.set(RobotConstants.minFlywheelThrottle);//Set intake spinning outward to push box out
    		RobotMap.rightFlywheel.set(RobotConstants.maxFlywheelThrottle);
     }
     
     public void stopFondle() { 	
-    	RobotMap.leftFlywheel.set(0);
+    	RobotMap.leftFlywheel.set(0);//Stop intake
 		RobotMap.rightFlywheel.set(0);
     	
     	
